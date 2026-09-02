ada606481cf27d4c07114483e1ed20852a8a8e83
danilokaminski-spec <danilo.kaminski@escola.pr.gov.br>
2026-09-02 09:44:09 -0300

    baixo cima

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const botaoTema = document.getElementById("botao_tema");
 
-botaoTema.addEventListener("click", () => {
+botaoTema.addEventListener("click", function () {
 
-document.body.classList.toggle("modo_escuro");
+  document.body.classList.toggle("modo_escuro");
 
-if (document.body.classList.contains("modo_escuro")) {
-botaoTema.textContent = "☀️";
-} else {
-botaoTema.textContent = "🌙";
-}
+  if (document.body.classList.contains("modo_escuro")) {
+    botaoTema.textContent = "☀️";
+  } else {
+    botaoTema.textContent = "🌙";
+  }
 
 });
